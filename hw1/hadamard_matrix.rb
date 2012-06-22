9b4cfd4ccc6347ff20c6f46575d6aced9200076f
ignisf <ignisf@bb624a11-7ba7-41a8-8378-4ab8e9ccd6b7>
2012-06-22 03:57:40 +0000

    Paley's Method complete

--- hw1/hadamard_matrix.rb
+++ hw1/hadamard_matrix.rb
@@ -3,12 +3,29 @@
 require 'matrix'
 
 class HadamardMatrix
+  H1 = Matrix[[1]]
+  H2 = Matrix[[1, 1],[1,-1]]
+  H8 =Matrix[[1, 1, 1, 1, 1, 1, 1, 1],
+             [1,-1, 1, 1,-1, 1,-1,-1],
+             [1,-1,-1, 1, 1,-1, 1,-1],
+             [1,-1,-1,-1, 1, 1,-1, 1],
+             [1, 1,-1,-1,-1, 1, 1,-1],
+             [1,-1, 1,-1,-1,-1, 1, 1],
+             [1, 1,-1, 1,-1,-1,-1, 1],
+             [1, 1, 1,-1, 1,-1,-1,-1]]
+  
   def HadamardMatrix.paley(order)
     raise "The given order #{order} is not valid" unless is_valid order
+    return H1 if order == 1
+    return H2 if order == 2
+    H8
   end
   
-  def HadamardMatrix.duplicate_order(order)
+  def HadamardMatrix.sylvester(order)
     raise "The given order #{order} is not valid" unless is_valid order
+    return H1 if order == 1
+    return H2 if order == 2
+    expand Q(order-1) - Matrix.I(order-1)
   end
   
   private
@@ -20,5 +37,22 @@ class HadamardMatrix
     return true  if order <= 2 
     return true  if Rational(order, 4).denominator == 1  
     return false
+  end
+  def self.Q(p)
+    Matrix.build(p, p) do |i, j|
+      chi(j - i, p)
+    end
+  end
+  def self.chi(a, p)
+    remainder = a.divmod(p)[1]
+    return  0 if remainder == 0
+    return  1 if (remainder & (remainder - 1)) == 0
+    return -1
+  end
+  def self.expand(matrix)
+    rows = matrix.to_a
+    rows.each {|row| row = row.to_a.insert(0, 1)}
+    rows.insert 0, Array.new(matrix.row_size + 1, 1)
+    Matrix.rows rows
   end
 end
--- hw1/spec/hadamard_matrix_spec.rb
+++ hw1/spec/hadamard_matrix_spec.rb
@@ -3,10 +3,9 @@ require './hadamard_matrix.rb'
 require 'matrix'
 
 shared_examples_for "a Hadamard matrix constructor" do
-  let (:hadamard) { create_instance.call size }
   let (:size) { reference.row_size }
-  let (:valid_size)   {[ 1, 2, 4, 8,12,16]}
-  let (:invalid_size) {[-2, 0, 3, 5, 6,10]}
+  let (:valid_order)   {[ 1, 2, 4, 8,12,16,28]}
+  let (:invalid_order) {[-2, 0, 3, 5, 6,10,14]}
   let (:reference) do
     Matrix[[1, 1, 1, 1, 1, 1, 1, 1],
            [1,-1, 1, 1,-1, 1,-1,-1],
@@ -17,10 +16,11 @@ shared_examples_for "a Hadamard matrix constructor" do
            [1, 1,-1, 1,-1,-1,-1, 1],
            [1, 1, 1,-1, 1,-1,-1,-1]]
   end
+  let (:hadamard) { create_instance.call size }
   
   context "when passed an invalid order" do
     it 'raises an error' do
-      invalid_size.each do |order|
+      invalid_order.each do |order|
         expect { create_instance.call order }.to raise_error
       end
     end
@@ -28,7 +28,7 @@ shared_examples_for "a Hadamard matrix constructor" do
   
   context "when passed a valid order" do
     it 'does not raise an error' do
-      valid_size.each do |order|
+      valid_order.each do |order|
         expect { create_instance.call order }.to_not raise_error
       end
     end
@@ -57,7 +57,7 @@ shared_examples_for "a Hadamard matrix constructor" do
 end
 
 describe HadamardMatrix do
-  it 'should not be instantiated' do
+  it 'cannot be instantiated' do
     HadamardMatrix.new.should raise_error
   end
   
@@ -66,8 +66,8 @@ describe HadamardMatrix do
     it_behaves_like "a Hadamard matrix constructor"
   end
   
-  describe '#duplicate_order' do
-    let (:create_instance) {Proc.new {|n| HadamardMatrix.duplicate_order n}}
+  describe '#sylvester' do
+    let (:create_instance) {Proc.new {|n| HadamardMatrix.sylvester n}}
     it_behaves_like "a Hadamard matrix constructor"
   end
 end
